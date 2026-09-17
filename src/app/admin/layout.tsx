5a15f46eea3aa49cff3e906cb1c53ab8cef6fb0c
Sheezylodhi <muhammadshahzaiblodhi@gmail.com>
2026-09-18 01:22:04 +0500

    layoutfix

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,6 +1,8 @@
 import AdminShell from "@/components/admin/AdminShell";
 import { requireAdmin } from "@/lib/auth";
 
+export const dynamic = "force-dynamic";
+
 export default async function AdminLayout({
   children,
 }: Readonly<{
