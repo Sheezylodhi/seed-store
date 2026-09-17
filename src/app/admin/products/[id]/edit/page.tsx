9a58cd2cee23db4411c05983ee99fbb6e0b5d965
Sheezylodhi <muhammadshahzaiblodhi@gmail.com>
2026-09-18 01:17:44 +0500

    //

--- src/app/admin/products/[id]/edit/page.tsx
+++ src/app/admin/products/[id]/edit/page.tsx
@@ -1,15 +1,5 @@
 import EditProductClient from "./EditProductClient";
 
-type PageProps = {
-  params: Promise<{
-    id: string;
-  }>;
-};
-
-export default async function EditProductPage({
-  params,
-}: PageProps) {
-  const { id } = await params;
-
-  return <EditProductClient id={id} />;
+export default function EditProductPage() {
+  return <EditProductClient />;
 }
